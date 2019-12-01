3fca1c90891de3fa5d9948b200c2e104f72ade58
Benjamin Akhras <b@akhras.at>
2019-12-01 01:10:41 +0100

    default dark

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-reveal-md git.md --theme simple --css style.css --absolute-url https://git.akhras.at --static --static-dirs=static
+reveal-md git.md --css style.css --absolute-url https://git.akhras.at --static --static-dirs=static
 
 reveal-md git.md --theme simple --css style.css --print _static/slides.pdf --print-size A4
 reveal-md git.md --css style.css --print _static/slides_dark.pdf --print-size A4
